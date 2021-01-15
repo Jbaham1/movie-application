a5fd6d23d3eae104ad8f9bbf5a1c37b2c11bc752
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-15 07:39:58 -0600

    movie title and image

--- movie-app.js
+++ movie-app.js
@@ -1,4 +1,5 @@
-document.onreadystatechange = function() {
+//loading
+document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
         document.querySelector("body").style.visibility = "hidden";
         document.querySelector("#loader").style.visibility = "visible";
@@ -7,4 +8,50 @@ document.onreadystatechange = function() {
         document.querySelector("body").style.visibility = "visible";
     }
 };
-$.ajax()
+//fetch
+const movieApiUrl = "https://antique-innate-coreopsis.glitch.me/movies"
+fetch(movieApiUrl).then(function (response) {
+    response.json().then(function (movies) {
+        console.log(movies)
+    })
+})
+
+const movieObj = {
+    "title": movies.title
+};
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movieObj),
+}
+
+$.ajax(movieApiUrl).done(function(data){
+    data.forEach(function (movie){
+       let movieHtml = "<tr>"
+        movieHtml += "<td>" + movie.title + "</td>"
+        movieHtml += `<td><img src='${movie.poster}'></td>`
+        movieHtml += "<td>" + movie.title + "</td></tr>"
+        $("#movies").append(movieHtml)
+    })
+})
+
+
+// const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
+// const url = '/posts';
+// const options = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(blogPost),
+// };
+// fetch(url, options)
+//     .then(/* post was created successfully */)
+//     .catch(/* handle errors */);
+
+
+
+
